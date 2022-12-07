2d08214b3643b9842bdbf20c62131030f294330c
ab3victoria <ab3victoria@gmail.com>
2022-12-07 19:28:40 +0200

    readme updated

---
+++ README.md
@@ -0,0 +1,58 @@
+
+# Beauty E-commerce App
+
+ This is an E-commerce website using the MERN stack.
+ 
+ The application loads products to a MongoDB database and displays them.
+
+ Users can click on a specific product to get more informationabout it(reviews,rating and price).
+ 
+ Users can add items to their shopping cart and place an order and make a payment (using [Paypal](https://developer.paypal.com/sdk/js/configuration/) as a payment gateway)
+ 
+ Some of the packages that are used: Muler, Morgan
+## Screenshots
+Home Page 
+
+![App Screenshot](https://i.postimg.cc/Y9LdcJws/b1.jpg)
+
+Cart
+
+![App cart](https://i.postimg.cc/nVv1vKVB/cart.jpg)
+
+Order Summary
+
+![App Screenshot](https://i.postimg.cc/hGfrxHht/order.jpg)
+
+Shipping information
+
+![App Screenshot](https://i.postimg.cc/8PZMHt2J/shipping.jpg)
+
+Admin Screen of products
+
+![App Screenshot](https://i.postimg.cc/9MPqqpPf/productslist.jpg)
+
+## Functionalities
+
+- Login and sign up to website
+- Update profile details
+- Read more information about a specific product
+- Add products to cart
+- Make multiple orders and track them
+- Purchase items with Paypal Sandbox. 
+ #### Admin Functioanlity:
+ * Fetching list of all products, users and orders.
+ * Update users details
+ * Update status of delievery
+ * Add, update and delete products
+ * Manage deliveries and orders
+ 
+## Tech Stack
+
+**Client:** React,Redux, [bootstrap](https://bootswatch.com/)
+
+**Server:** Node, Express, MongoDB, PayPal Node SDK
+
+
+## Deployment
+
+- Deployed to [Heroku](https://dashboard.heroku.com/)
---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Blithe, Pressed Serum, Tundra Chaga',
+    image: '/images/image1.jpg',
+    description:
+    'Pressed Serum Tundra Chaga is a high-nutrition serum that provides rich moisture and nutritions to skin, as it is packed with 59.4% of tundra chaga mushroom extracts',
+    brand: 'Blithe',
+    category: 'Serum',
+    price: 13.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Low pH Good Morning Gel Cleanser',
+    image: '/images/image7.jpg',
+    description:
+    'Formulated with botanical skin-purifying ingredients, mildly acidic cleanser works softly to make skin supple and clear. (For sensitive skin, Morning time cleanser).',
+    brand: 'The Ordinary',
+    category: 'Face Wash & Cleansers',
+    price: 59.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Clarifying Treatment Toner',
+    image: '/images/image3.jpg',
+    description:
+    'With low pH formula made with purifying botanical ingredients, this toner refresh, soothe, and soften skin.',
+    brand: 'Cosrx',
+    category: 'Toners',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Idealove, So Soothe Me, Intensive Green Tea Toner',
+    image: '/images/image4.jpg',
+    description:
+      'After cleansing, apply a small amount with the palms of your hands or with a cotton swab. Gently press into skin until fully absorbed. For all skin types, especially sensitive.',
+    brand: 'Idealove',
+    category: 'Toners',
+    price: 19.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Idealove, You had me at Aloe, Aloe Propolis Hydrating Gel',
+    image: '/images/image5.jpg',
+    description:
+      'Even those with oily skin can struggle to keep skin hydrated. Idealove™ You Had Me at Aloe, Aloe Propolis Hydrating Gel is the perfect solution to hydrate and refresh the appearance of skin without adding excess oil, helping you achieve a soft, supple complexion.',
+    brand: 'Logitech',
+    category: 'Face Wash & Cleansers',
+    price: 22.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Cosrx, Oil-Free Ultra-Moisturizing Lotion with Birch Sap',
+    image: '/images/image7.jpg',
+    description:
+      'Formulated with 70.8% Birch Sap (Betula Platyphylla Japonica Juice), this lotion helps hydrate and calm the skin.',
+    brand: 'Cosrx',
+    category: 'Face Wash & Cleansers',
+    price: 19.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
